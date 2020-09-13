dd7e34cf9503b63c0216a2fff99bc2844a7d4bca
zhu-ch <43909561+zhu-ch@users.noreply.github.com>
2020-09-13 16:31:27 +0800

    优化返回值

--- src/main/java/bit/ss/surveysystem/modules/sys/Controller/DictController.java
+++ src/main/java/bit/ss/surveysystem/modules/sys/Controller/DictController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
-      * @Description 字典管理api
-      * @author wh
-      * @date 2019/8/12 13:08
-      */
+ * @author wh
+ * @Description 字典管理api
+ * @date 2019/8/12 13:08
+ */
 @RequestMapping("api/sys/dict")
 @Controller
 public class DictController extends BaseApi {
@@ -33,81 +33,81 @@ public class DictController extends BaseApi {
 //            return retMsg.Set(MsgType.ERROR);
 //    }
     //获取字典类型序列
-    @RequestMapping(value= "getDictTypeList",method = RequestMethod.GET)
+    @RequestMapping(value = "getDictTypeList", method = RequestMethod.GET)
     @ResponseBody
-    public Object getDictTypeList()throws Exception {
+    public Object getDictTypeList() throws Exception {
         try {
             List<String> data = dictService.selectDictTypeList();
-            return retMsg.Set(MsgType.SUCCESS,data);
-        }catch (Exception e){
+            return retMsg.Set(MsgType.SUCCESS, data);
+        } catch (Exception e) {
             e.printStackTrace();
-            return retMsg.Set(MsgType.ERROR);
+            return retMsg.Set(MsgType.ERROR, e.toString());
         }
     }
 
 
-    @RequestMapping(value ="selectDictListByPage",method = RequestMethod.POST)
+    @RequestMapping(value = "selectDictListByPage", method = RequestMethod.POST)
     @ResponseBody
-    public Object selectDictListByPage(@RequestBody Dict dict)throws Exception {
-            try{
-                Page<Dict> page = new Page<>();
-                page.setResultList(dictService.selectDictListByPage(dict));
-                page.setTotal(dictService.selectSearchCount(dict));
-                return retMsg.Set(MsgType.SUCCESS,page);
-            }catch (Exception e){
-                e.printStackTrace();
-                return  retMsg.Set(MsgType.ERROR);
-            }
+    public Object selectDictListByPage(@RequestBody Dict dict) throws Exception {
+        try {
+            Page<Dict> page = new Page<>();
+            page.setResultList(dictService.selectDictListByPage(dict));
+            page.setTotal(dictService.selectSearchCount(dict));
+            return retMsg.Set(MsgType.SUCCESS, page);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return retMsg.Set(MsgType.ERROR, e.toString());
+        }
     }
 
-    @RequestMapping(value = "insertOrUpdateDict",method = RequestMethod.POST)
+    @RequestMapping(value = "insertOrUpdateDict", method = RequestMethod.POST)
     @ResponseBody
-    public Object insertOrUpdateDict(@RequestBody Dict dict)throws Exception {
-        try{
-            if("".equals(dict.getId())||dict.getId()==null){
-                if(dictService.insertDict(dict))
+    public Object insertOrUpdateDict(@RequestBody Dict dict) throws Exception {
+        try {
+            if ("".equals(dict.getId()) || dict.getId() == null) {
+                if (dictService.insertDict(dict))
                     return retMsg.Set(MsgType.SUCCESS);
-                else return retMsg.Set(MsgType.WARNING,"未更新任何数据");
-            }else{
-                if(dictService.updateDict(dict))
+                else return retMsg.Set(MsgType.WARNING, "未更新任何数据");
+            } else {
+                if (dictService.updateDict(dict))
                     return retMsg.Set(MsgType.SUCCESS);
-                else return retMsg.Set(MsgType.WARNING,"未更新任何数据");
+                else return retMsg.Set(MsgType.WARNING, "未更新任何数据");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            return  retMsg.Set(MsgType.ERROR);
+            return retMsg.Set(MsgType.ERROR, e.toString());
         }
 
 
     }
 
-    @RequestMapping(value="deleteDictByIds",method = RequestMethod.POST)
+    @RequestMapping(value = "deleteDictByIds", method = RequestMethod.POST)
     @ResponseBody
-    public Object deleteDictByIds(@RequestBody List<Dict> dicts)throws Exception {
-        try{
-            if(dictService.deleteDictByIds(dicts)){
+    public Object deleteDictByIds(@RequestBody List<Dict> dicts) throws Exception {
+        try {
+            if (dictService.deleteDictByIds(dicts)) {
                 return retMsg.Set(MsgType.SUCCESS);
-            }else
-                return retMsg.Set(MsgType.WARNING,"未更新任何数据");
-        }catch (Exception e){
+            } else
+                return retMsg.Set(MsgType.WARNING, "未更新任何数据");
+        } catch (Exception e) {
             e.printStackTrace();
-            return retMsg.Set(MsgType.ERROR);
+            return retMsg.Set(MsgType.ERROR, e.toString());
         }
     }
 
-    @RequestMapping(value="deleteDictById",method = RequestMethod.POST)
+    @RequestMapping(value = "deleteDictById", method = RequestMethod.POST)
     @ResponseBody
-    public Object deleteDictById(@RequestParam String dicts)throws Exception {
-        Dict dict  = new Dict();
+    public Object deleteDictById(@RequestParam String dicts) throws Exception {
+        Dict dict = new Dict();
         dict.setId(dicts);
-        try{
-            if(dictService.deleteDictById(dict)){
+        try {
+            if (dictService.deleteDictById(dict)) {
                 return retMsg.Set(MsgType.SUCCESS);
-            }else
-                return retMsg.Set(MsgType.WARNING,"未更新任何数据");
-        }catch (Exception e){
+            } else
+                return retMsg.Set(MsgType.WARNING, "未更新任何数据");
+        } catch (Exception e) {
             e.printStackTrace();
-            return retMsg.Set(MsgType.ERROR);
+            return retMsg.Set(MsgType.ERROR, e.toString());
         }
     }
 
